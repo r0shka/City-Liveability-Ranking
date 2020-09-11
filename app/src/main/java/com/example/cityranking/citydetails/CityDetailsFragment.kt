6a2ae92d0b2f17f2a50d6b2dfc955e5e37937f60
Vadym Roshka <vadym.roshka@runtastic.com>
2020-09-11 17:32:15 +0200

    chart wip

--- app/src/main/java/com/example/cityranking/citydetails/CityDetailsFragment.kt
+++ app/src/main/java/com/example/cityranking/citydetails/CityDetailsFragment.kt
@@ -13,6 +13,7 @@ import com.example.cityranking.data.City
 import com.example.cityranking.databinding.FragmentCityDetailsBinding
 import com.example.cityranking.utilities.IMAGE_SIZE_MEDIUM
 import com.example.cityranking.utilities.Utils
+import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
@@ -22,12 +23,11 @@ import kotlinx.android.synthetic.main.fragment_city_details.*
 
 class CityDetailsFragment : Fragment() {
 
-    private var chart = city_details_chart
-
     private val sharedViewModel: SharedViewModel by activityViewModels()
 
     private var _binding: FragmentCityDetailsBinding? = null
     private val binding get() = _binding!!
+    private var city: City? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,40 +39,43 @@ class CityDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val city = arguments?.getParcelable<City>("city")
+        city = arguments?.getParcelable("city")
         binding.cityDetailsName.text = "${city?.name}, ${city?.country}"
         GlideApp.with(this)
             .load(Utils.getImageRef(city?.id, IMAGE_SIZE_MEDIUM))
             .into(binding.cityDetailsThumbnail)
         setupChart()
+        setData()
     }
 
     private fun setupChart(){
-        chart.setViewPortOffsets(0F, 0F, 0F, 0F);
-        chart.setBackgroundColor(Color.rgb(104, 241, 175));
-        chart.description.isEnabled = false
-        chart.setTouchEnabled(false)
-        chart.isDragEnabled = false
-        chart.setScaleEnabled(false)
-        chart.setPinchZoom(false)
-        chart.setDrawGridBackground(false)
-        chart.legend.isEnabled = false
+        city_details_chart.setViewPortOffsets(0F, 0F, 0F, 0F);
+        city_details_chart.setBackgroundColor(Color.rgb(31, 128, 255));
+        city_details_chart.description.isEnabled = false
+        city_details_chart.setTouchEnabled(false)
+        city_details_chart.isDragEnabled = false
+        city_details_chart.setScaleEnabled(false)
+        city_details_chart.setPinchZoom(false)
+        city_details_chart.setDrawGridBackground(false)
+        city_details_chart.legend.isEnabled = false
     }
 
-    private fun setData(count: Int, range: Float) {
+    private fun setData() {
         val values: ArrayList<Entry> = ArrayList()
-        for (i in 0 until count) {
-            val value = (Math.random() * (range + 1)).toFloat() + 20
-            values.add(Entry(value, i.toFloat()))
+        val mercer: ArrayList<Pair<String?,Int?>> = ArrayList()
+        city?.mercer?.forEach{ (key,value) -> mercer.add(Pair(key,value))}
+
+        mercer.forEach {
+            values.add(Entry(it.first!!.toFloat(), it.second!!.toFloat()))
         }
         val set1: LineDataSet
-        if (chart.getData() != null &&
-            chart.getData().getDataSetCount() > 0
+        if (city_details_chart.data != null &&
+            city_details_chart.data.dataSetCount > 0
         ) {
-            set1 = chart.getData().getDataSetByIndex(0)
+            set1 = city_details_chart.data.getDataSetByIndex(0) as LineDataSet
             set1.setValues(values)
-            chart.getData().notifyDataChanged()
-            chart.notifyDataSetChanged()
+            city_details_chart.getData().notifyDataChanged()
+            city_details_chart.notifyDataSetChanged()
         } else {
             // create a dataset and give it a type
             set1 = LineDataSet(values, "DataSet 1")
@@ -87,18 +90,18 @@ class CityDetailsFragment : Fragment() {
             set1.color = Color.WHITE
             set1.fillColor = Color.WHITE
             set1.fillAlpha = 100
-            set1.setDrawHorizontalHighlightIndicator(false)
+            set1.setDrawHorizontalHighlightIndicator(true)
             set1.fillFormatter =
-                IFillFormatter { dataSet, dataProvider -> chart.getAxisLeft().getAxisMinimum() }
+                IFillFormatter { dataSet, dataProvider -> city_details_chart.getAxisLeft().getAxisMinimum() }
 
             // create a data object with the data sets
             val data = LineData(set1)
-            data.setValueTypeface(tfLight)
+            //data.setValueTypeface(tfLight)
             data.setValueTextSize(9f)
-            data.setDrawValues(false)
+            data.setDrawValues(true)
 
             // set data
-            chart.setData(data)
+            city_details_chart.setData(data)
         }
     }
 
